f5328af2fd0a8291b161172b2d96d709202274d9
Thomas Clement <Thomasaclement98@gmail.com>
2022-11-04 15:49:15 -0400

    init

---
+++ test/h20.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.15;
+
+import "foundry-huff/HuffDeployer.sol";
+import "forge-std/Test.sol";
+import "forge-std/console.sol";
+
+contract H20Test is Test {
+    
+
+    function testGetOwner() public {
+        console.log("Hello");
+    }
+
+}
